873c6d74da4b3b2513e1e9c7396d72ac4ced718b
David Mjomba <smodavprivate@gmail.com>
2022-04-26 06:42:23 +0300

    switches to v2 endpoints

--- src/Mpesa/Repositories/Endpoint.php
+++ src/Mpesa/Repositories/Endpoint.php
@@ -4,12 +4,19 @@ namespace SmoDav\Mpesa\Repositories;
 
 class Endpoint
 {
-    const MPESA_AUTH     = 'oauth/v1/generate?grant_type=client_credentials';
-    const MPESA_ID_CHECK = 'mpesa/checkidentity/v1/query';
-    const MPESA_REGISTER = 'mpesa/c2b/v1/registerurl';
-    const MPESA_SIMULATE = 'mpesa/c2b/v1/simulate';
-    const MPESA_LNMO = 'mpesa/stkpush/v1/processrequest';
-    const MPESA_LNMO_VALIDATE = 'mpesa/stkpushquery/v1/query';
-    const CUSTOMER_PAYBILL_ONLINE = 'CustomerPayBillOnline';
-    const CUSTOMER_BUYGOODS_ONLINE = 'CustomerBuyGoodsOnline';
+    public const MPESA_AUTH = 'oauth/v1/generate?grant_type=client_credentials';
+
+    public const MPESA_ID_CHECK = 'mpesa/checkidentity/v1/query';
+
+    public const MPESA_REGISTER = 'mpesa/c2b/v2/registerurl';
+
+    public const MPESA_SIMULATE = 'mpesa/c2b/v2/simulate';
+
+    public const MPESA_LNMO = 'mpesa/stkpush/v1/processrequest';
+
+    public const MPESA_LNMO_VALIDATE = 'mpesa/stkpushquery/v1/query';
+
+    public const CUSTOMER_PAYBILL_ONLINE = 'CustomerPayBillOnline';
+
+    public const CUSTOMER_BUYGOODS_ONLINE = 'CustomerBuyGoodsOnline';
 }
